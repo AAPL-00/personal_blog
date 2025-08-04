dc48e666c67d9aee1b0700d34bc04c22ea3572f9
aapl00 <aapl00@proton.me>
2025-08-04 10:05:21 -0400

    Solve models bug and add PasswordUpdateSerializer

--- users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class CustomUser(models.CustomUser):
-
-    def __str__(self):
-        return self.username
--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import User
 
-class CustomUserSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email',]
 
 class CreateUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only =True, allow_blank=True)
+    password = serializers.CharField(write_only =True, allow_blank=False)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
         extra_kwargs = {
             'username': {'required': True, 'allow_blank': False},
@@ -19,23 +20,40 @@ class CreateUserSerializer(serializers.ModelSerializer):
         }
 
     def validate_username(self, value):
-        if CustomUser.objects.filter(user=value).exists():
+        if User.objects.filter(username=value).exists():
             raise serializers.ValidationError("El nombre de usuario ya está en uso")
         return value
-    
+
     def validate_email(self, value):
-        if CustomUser.objects.filter(email=value).exists():
+        if User.objects.filter(email=value).exists():
             raise serializers.ValidationError("El correo ya está en uso")
         return value
-    
+
     def validate_password(self, value):
         validate_password(value, user=None)
         return value
-    
+
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = User.objects.create_user(
             username = validated_data['username'],
             email = validated_data['email'],
             password = validated_data['password']
         )
         return user
+
+
+class PasswordUpdateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=True, allow_blank=False, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['password']
+
+    def validate_password(self, value):
+        validate_password(value, user=self.instance)
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
