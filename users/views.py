39496a8cffccdedc5ce7ca750e9622dbe57e9082
aapl00 <aapl00@proton.me>
2025-08-04 10:06:26 -0400

    Add UserView, CreateUserView and UpdatePasswordView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from users.serializers import UserSerializer, CreateUserSerializer, PasswordUpdateSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UserView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+
+class PasswordUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PasswordUpdateSerializer
+
+    def get_object(self):
+        return self.request.user
